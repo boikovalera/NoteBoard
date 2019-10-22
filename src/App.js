bc82b7baaa00d193f45db9ac4a049a49af4d8441
Бойко Валерий Михайлович <valerij.bojko@privatbank.ua>
2019-10-22 16:31:19 +0300

    Добавление компонентов

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import AddNoteComponent from './AddNoteComponent/AddNoteComponent'
+import NoteListComponent from './NoteListComponent/NoteListComponent'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style=''>
+      <h1>Note board</h1>
+      <AddNoteComponent/>
+      <NoteListComponent/>      
     </div>
   );
 }
